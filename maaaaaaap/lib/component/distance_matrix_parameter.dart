f29761b2edfb13ff6f486529fa21b4ab1283395a
StarsinLiver <you@example.com>
2024-04-21 01:43:39 +0900

    flutter

--- maaaaaaap/lib/component/distance_matrix_parameter.dart
+++
@@ -1,9 +0,0 @@
-class DistanceMatrixParameter {
-  double startLatitude;
-  double startLongitude;
-  double endLatitude;
-  double endLongitude;
-
-  DistanceMatrixParameter({required this.startLatitude,required this.startLongitude,required this.endLatitude,
-    required this.endLongitude});
-}
